c7a0019bf5082fdf7d93d92607cf233bc81110ff
fromtheplace <blamboxcity@gmail.com>
2026-02-04 17:25:25 +1300

    Add files via upload

---
+++ js/projects-data.js
@@ -0,0 +1,276 @@
+const projectData = {
+  "projects": {
+    "1": {
+      "title": "Ōtepoti Hip Hop Hustle 24",
+      "youtube": "AyBXBHIu_Vc",
+      "playlist": "PL2HwIIvWJIPHkofbvzfQy7apKM2vYth5k",
+      "chips": [
+        {
+          "type": "youtube",
+          "id": "J06W1MAzugI",
+          "title": "Shorts/Reels"
+        },
+        {
+          "type": "url",
+          "url": "https://www.odt.co.nz/news/dunedin/connecting-through-music",
+          "title": "Press Article #1"
+        },
+        {
+          "type": "url",
+          "url": "https://www.odt.co.nz/news/arts-festival-dunedin/night-%E2%80%98boogieing%E2%80%99-hip-hop-event-hit",
+          "title": "Press Article #2"
+        }
+      ],
+      "images": [
+        "images/Nook_banner.jpg",
+        "images/nook_poster.jpg"
+      ],
+      "description": "Multicam live video production, live camera projection, live audio mastering, after film production, Long and short-form video publishing.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Multicam live video production</li>\n        <li>Live on-stage camera projection</li>\n        <li>Live Audio Mastering</li>\n        <li>After-Film Production</li>\n        <li>Long-Form & Short-Form Video Publishing</li>\n      </ul>\n    </div>",
+      "html_badge": "Video / Production",
+      "html_image": "images/OHHH+2024+JLP.jpg",
+      "html_h4": "Ōtepoti Hip Hop Hustle",
+      "html_description": "Music Festival videography and live production."
+    },
+    "2": {
+      "youtube": "Ct2avwGJqjA",
+      "startTime": 0,
+      "title": "Music for People",
+      "chips": [
+        {
+          "type": "youtube",
+          "id": "XvE0GewwNlI",
+          "title": "St Pauls Cathedral show",
+          "startTime": 18806
+        },
+        {
+          "type": "youtube",
+          "id": "HWOwgh6AI5k",
+          "title": "Starters Bar show",
+          "startTime": 6899
+        },
+        {
+          "type": "url",
+          "url": "https://www.odt.co.nz/entertainment/music/powerful-brew-something-new",
+          "title": "ODT Article #1"
+        },
+        {
+          "type": "url",
+          "url": "https://www.critic.co.nz/news/article/9500/the-spat-behind-music-for-the-peoples-relocation-f",
+          "title": "Critic/Te Ārohi Article"
+        },
+        {
+          "type": "url",
+          "url": "https://www.odt.co.nz/entertainment/music/people-get-ready",
+          "title": "ODT Article #2"
+        },
+        {
+          "type": "url",
+          "url": "https://www.facebook.com/events/197513774778177",
+          "title": "MFP FB Event"
+        },
+        {
+          "type": "url",
+          "url": "https://www.facebook.com/events/2904413966508867",
+          "title": "MFP2 FB Event"
+        }
+      ],
+      "images": [
+        "images/mfp1_2.jpg",
+        "images/MFP234.jpg",
+        "images/mfp0.jpg"
+      ],
+      "description": "In February 2020, just before lockdowns, New New New hosted its largest and most ambitious line-up to date. Music for People featured 11 genre-spanning acts, from rap to rock and everything in between. The follow-up expanded to 32 acts across two distinct sessions: an afternoon in St Paul's Cathedral and an evening at Starters Bar. <br/>  <strong><em>From the Place</em></strong>  led both the creative and technical direction, developing a cohesive visual identity and a multi-platform production workflow. This included multicam live video streaming for both sessions, real-time on-site camera projection, and live audio mastering for high-fidelity broadcast and archival delivery. All graphic assets—across print, digital, and motion—were designed in-house, along with a custom ecommerce and ticketing platform featuring artist profiles, schedules, and sales integration. The result was a seamless, hybrid production that bridged audiences across spaces and styles, delivering a unified experience in both physical and digital form.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Multicam Live Video Production</li>\n        <li>Live Audio Mastering</li>\n        <li>Graphic Design</li>\n        <li>Ecommerce/Ticketing website</li>\n        <li>Music Compostion/Teaser Video Production</li>\n      </ul>\n    </div>",
+      "html_badge": "Design / Video / Stream / Stage",
+      "html_image": "images/mfp0.jpg",
+      "html_h4": "Music for People",
+      "html_description": "Multi-day crowd-sourced music event with coordination across multiple venues and digital platforms."
+    },
+    "3": {
+      "title": "Nook & Cranny Music Fest",
+      "youtube": "WImSgUXMGC0",
+      "description": "Multicam live video production, live camera projection, live audio mastering, after film production, Long and short-form video publishing.",
+      "chips": [
+        {
+          "type": "youtube",
+          "id": "bs1pQGatwVw",
+          "title": "View stream Pt.1",
+          "startTime": 9178
+        },
+        {
+          "type": "youtube",
+          "id": "ZjQX8IGXmkk",
+          "title": "View stream Pt.2",
+          "startTime": 3344
+        },
+        {
+          "type": "twitch",
+          "title": "Lockdown Stream Pt.1",
+          "videoId": "616649181"
+        },
+        {
+          "type": "twitch",
+          "title": "Lockdown Stream Pt.2",
+          "videoId": "616649388"
+        }
+      ],
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Multicam Live Video Production</li>\n        <li>Live Camera Projection</li>\n      </ul>\n    </div>",
+      "images": [
+        "images/Nook_banner.jpg",
+        "images/bssc.jpg",
+        "images/ncmtv.jpg",
+        "images/nook_poster.jpg"
+      ],
+      "html_badge": "Design / Video / Stream",
+      "html_image": "images/178799204_3729780010482660_6832248583617810706_n.jpg",
+      "html_h4": "Nook & Cranny Music Fest",
+      "html_description": "Livestreamed Music festival in a library"
+    },
+    "4": {
+      "title": "Waitati Music Festival",
+      "youtube": "yxH2mg92doY",
+      "startTime": 4,
+      "chips": [
+        {
+          "type": "youtube",
+          "id": "3OS_XOca06o",
+          "title": "Undertow set"
+        },
+        {
+          "type": "youtube",
+          "id": "f8fhAe6YrTw",
+          "title": "Ferdishenko set"
+        },
+        {
+          "type": "url",
+          "url": "https://www.odt.co.nz/the-star/music-festival-organisers-taking-cautious-approach",
+          "title": "Press Article #1"
+        },
+        {
+          "type": "twitch",
+          "title": "Livestream",
+          "videoId": "596395581"
+        },
+        {
+          "type": "url",
+          "url": "https://www.facebook.com/events/3369191529775528/",
+          "title": "Facebook Event"
+        }
+      ],
+      "description": "Initially Poster/campaign design work lead to collaborating to create and impromptu online music fest.\n\nDuring the pandemic, livestreaming grew popular. Waitati Music Festival was the first local event streamed, aggregating all the artists perfomances into a single stream. From the place managed all \nfacets of promotion and admin, through to producing live mastered audio and on screen display within a seemless broadcast - saving the event's continutiy and providng a production that stood out through the lockdown.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Campaign Design</li>\n        <li>Livestream production</li>\n\t <li>On Screen graphics</li>\n      </ul>\n    </div>",
+      "html_badge": "Design / Video / Stream",
+      "html_image": "images/wmfl.jpg",
+      "html_h4": "Waitati Music Festival",
+      "html_description": "Brand & campaign design, Livestream production",
+      "images": [
+        "images/wmfl_sets.png",
+        "images/wmf20.jpg"
+      ]
+    },
+    "5": {
+      "youtube": "iCHbYu7owBQ",
+      "title": "Autumn Arena",
+      "description": "Description for project five. Brief summary of services and highlights.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Video Production</li>\n        <li>Composition (EXPJ - Bass Guitar)</li>\n        <li>Graphic Design</li>\n        <li>Event Management (Booking Administrator)</li>\n      </ul>\n    </div>",
+      "html_badge": "Design / Video / Stage",
+      "html_image": "images/arena_cover.jpg",
+      "html_h4": "Arena Project",
+      "html_description": "Music & Performance"
+    },
+    "6": {
+      "youtube": "ox4QqeyNykQ",
+      "title": "Ollie Crooks - Take you There (LP)",
+      "description": "Description for project six. Event recap, highlights, and interviews.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Lighting Design</li>\n        <li>Stage Management</li>\n      </ul>\n    </div>",
+      "html_badge": "Recording / Design / Video",
+      "html_image": "images/OllieCrooks_cover2.png",
+      "html_h4": "Ollie Crooks - Take You There (LP)",
+      "html_description": "Album recording and video production",
+      "chips": [
+        {
+          "type": "iframe",
+          "title": "Player",
+          "iframe": "<iframe style=\"border: 0; width: 350px; height: 470px;\" src=\"https://bandcamp.com/EmbeddedPlayer/album=439202460/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/\" seamless><a href=\"https://olliecrooks.bandcamp.com/album/take-you-there\">Take you there by Ollie Crooks</a></iframe>"
+        },
+        {
+          "type": "youtube",
+          "id": "wVFA4gm0K8Q",
+          "title": "Lobofest Set"
+        }
+      ]
+    },
+    "7": {
+      "youtube": "DPQRV7qTxT0",
+      "title": "Fonterra - Infographic Video Production",
+      "description": "For this project was created to commemorate 100 years of lactose production in New Zealand.\n\nWork included Storyboard planning, 2D graphic creation, 2D Animation, Soundtrack composition and production.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Animation design & production</li>\n        <li>Music Composition</li>\n        <li>Voice over production</li>\n      </ul>\n    </div>",
+      "html_badge": "Video / Audio / Design",
+      "html_image": "images/fonterra.jpg",
+      "html_h4": "Fonterra - Infographic Video",
+      "html_description": "Infographic production"
+    },
+    "8": {
+      "youtube": "Rc7OHXJtWco",
+      "title": "from the place - Youtube Channel",
+      "description": "Documentation and cataloging performance art.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>\n      <ul>\n        <li>Sound Design</li>\n        <li>Color Correction</li>\n      </ul>\n    </div>",
+      "html_image": "images/ftpyt1_w.jpg",
+      "html_h4": "from the place - Youtube Channel",
+      "html_description": "Sample description for project eight.",
+      "html_badge": "Video / Audio",
+      "images": [
+        "images/ftpyt1_w.jpg"
+      ]
+    },
+    "9": {
+      "html_image": "https://via.placeholder.com/300x200",
+      "html_h4": "Print and screen design",
+      "html_description": "Print and screen design examples.",
+      "html_badge": "Screen / Print",
+      "title": "Print and screen design"
+    },
+    "10": {
+      "html_image": "https://via.placeholder.com/300x200",
+      "html_h4": "Newsyland",
+      "html_description": "Sample description for project ten.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>"
+    },
+    "11": {
+      "html_image": "images/spaceconcert.jpg",
+      "html_h4": "Space Concert",
+      "html_description": "Sample description for project eleven.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>"
+    },
+    "12": {
+      "html_image": "images/tk2.jpg",
+      "html_h4": "Te Karaka - Te Runanga O Ngai Tahu",
+      "html_description": "Sample description for project twelve.",
+      "creditsHTML": "<div class=\"credit-list\">\n      <h2>Scope</h2>      <ul>\n        <li>Layout & advertising design</li>\n      </ul>\n    </div>",
+      "title": "Te Karaka Magazine"
+    },
+    "13": {
+      "html_badge": "Design / Video / Stage",
+      "html_image": "images/jsfcover.jpg",
+      "html_h4": "JAH SUN: FEST",
+      "title": "JAH SUN: FEST",
+      "youtube": "iYsvXeUsH0E"
+    }
+  },
+  "project_order": [
+    "4",
+    "1",
+    "6",
+    "7",
+    "3",
+    "2",
+    "12",
+    "5",
+    "8",
+    "9",
+    "10",
+    "11",
+    "13"
+  ]
+};
